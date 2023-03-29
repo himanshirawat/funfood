15edba2c3b2fcb6b9873dad25ea426fc56ad0ea8
Himanshika Rawat <himanshirawat604@gmail.com>
2023-03-29 12:16:56 +0530

    login with authentication completed #8

--- backend/Routes/CreateUser.js
+++ backend/Routes/CreateUser.js
@@ -3,31 +3,76 @@ const router = express.Router()
 const User = require("../models/User")
 const { body, validationResult } = require('express-validator');
 
-router.post("/createuser",[
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+const jwtSecret = "mynameishimanshirawat18012003hello"
 
-body('email').isEmail(),
-body('name').isLength({ min: 5 }),
-body('password','Incorrect Password').isLength({ min: 8 })],
 
-async(req, res)=>{
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
-    }
 
-    try {
-        await User.create({
-            name: req.body.name,
-            password:req.body.password,
-            email:req.body.email,
-            location:req.body.location
-        })
-    res.json({success:true});
+router.post("/createuser", [
 
-    } catch (error) {
-        console.log(error)
-        res.json({success:false});
-    }
-})
+    body('email').isEmail(),
+    body('name').isLength({ min: 5 }),
+    body('password', 'Incorrect Password').isLength({ min: 8 })],
+
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+        const salt = await bcrypt.genSalt(10);
+        let secPassword  = await bcrypt.hash(req.body.password, salt)
+        try {
+            await User.create({
+                name: req.body.name,
+                password: secPassword,
+                email: req.body.email,
+                location: req.body.location
+            })
+            res.json({ success: true });
+
+        } catch (error) {
+            console.log(error)
+            res.json({ success: false });
+        }
+    })
+
+router.post("/loginuser", [
+
+    body('email').isEmail(),
+    body('password', 'Incorrect Password').isLength({ min: 8 })],
+
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+        let email = req.body.email;
+        try {
+            let userData = await User.findOne({email});
+            if (!userData) {
+                return res.status(400).json({ errors: "Try logging with correct credentials" })
+            }
+
+            const pwdCompare = await bcrypt.compare(req.body.password,userData.password)
+            if (!pwdCompare) {
+                return res.status(400).json({ errors: "Try logging with correct credentials" })
+            }
+            const data ={
+                user:{
+                    id:userData.id
+                }
+            }
+            const authToken = jwt.sign(data,jwtSecret)
+            return res.json({ success: true , authToken:authToken})
+
+
+        } catch (error) {
+            console.log(error)
+            res.json({ success: false });
+        }
+    })
 
 module.exports = router;
