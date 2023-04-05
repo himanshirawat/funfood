0303387b04d06e08c1bbccd3c244153588c1eab1
Himanshika Rawat <himanshirawat604@gmail.com>
2023-04-05 10:47:29 +0530

    changes

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 5000
+const port = 5000 || process.env.PORT
 const mongoconnect = require('./db')
 
 app.use((req,res,next)=>{
