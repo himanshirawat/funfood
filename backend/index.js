43397bf3ba2842a1326ee2c3ff0ff5d59667996f
Himanshi Rawat <himanshirawat604@gmail.com>
2023-08-02 16:43:00 +0530

    change

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,12 @@ app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header('Access-Control-Allow-Credentials','true');
+
+  if (req.method === 'OPTIONS'){
+    return res.status(204).end();
+  }
+  
   next();
 });
 
